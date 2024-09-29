2aa45339ca84a80565819a186049462030af3c4b
Srujith <srujith@kudikala.com>
2024-09-29 11:22:27 -0400

    rename backend

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,4 @@
-from brainstorm_speedrun import create_app
+from brainstorm_shuffle import create_app
 
 application = create_app({'SQLALCHEMY_DATABASE_URI': 'sqlite:///brainstorm.db',
     'SQLALCHEMY_TRACK_MODIFICATIONS': False})
