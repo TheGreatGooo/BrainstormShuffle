af8fe502ee42184cda42a6fcf81f0ac1e7b72ee9
Srujith <srujith@kudikala.com>
2024-09-29 11:31:37 -0400

    make sure to pack the FE

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+pushd frontend/brainstorm-shuffle-app
+npm run build
+popd
 podman build -f Dockerfile.backend -t local/brainstorm-shuffle-backend
 podman build -f Dockerfile.nginx -t local/brainstorm-shuffle-nginx
-podman run -p 8000:8000 local/brainstorm-shuffle-backend
-podman run -p 8080:80 local/brainstorm-shuffle-nginx
+podman run -d -p 8000:8000 local/brainstorm-shuffle-backend
+podman run -d -p 8080:80 local/brainstorm-shuffle-nginx
