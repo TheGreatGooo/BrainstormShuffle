310d73b47a4b621dee0af21b26240afaf242ec28
srujith <srujith@kudikala.com>
2024-09-30 04:18:47 -0400

    update to use volumes

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ npm run build
 popd
 podman build -f Dockerfile.backend -t local/brainstorm-shuffle-backend
 podman build -f Dockerfile.nginx -t local/brainstorm-shuffle-nginx
-podman run -d -p 8000:8000 --name brainstorm-shuffle-backend local/brainstorm-shuffle-backend
-podman run -d -p 8080:80 --name brainstorm-shuffle-nginx local/brainstorm-shuffle-nginx
+podman run -d -p 8000:8000 -v ./backend:/app --name brainstorm-shuffle-backend local/brainstorm-shuffle-backend
+podman run -d -p 8080:80 -v ./frontend/brainstorm-shuffle-app/dist:/config/www -v ./nginx.conf:/config/nginx/site-confs/default.conf --name brainstorm-shuffle-nginx local/brainstorm-shuffle-nginx
+
