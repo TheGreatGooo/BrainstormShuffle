fe62eb1ff4f7c4002f6f61b6665f3781b7bd5543
srujith <srujith@kudikala.com>
2024-09-30 07:26:04 -0400

    console out error messages

--- frontend/brainstorm-shuffle-app/src/App.js
+++ frontend/brainstorm-shuffle-app/src/App.js
@@ -35,8 +35,10 @@ const App = () => {
     } catch (error) {
       if (error.response){
         setErrorMsg(error.response.data.msg);
+        console.log(error.response.data.msg);
       }else{
-        setErrorMsg("Something bad happened: " + error)
+        setErrorMsg("Something bad happened: " + error);
+        console.log("Something bad happened: " + error);
       }
     }
   };
@@ -51,8 +53,10 @@ const App = () => {
     } catch (error) {
       if (error.response){
         setErrorMsg(error.response.data.msg);
+        console.log(error.response.data.msg);
       }else{
-        setErrorMsg("Something bad happened: " + error)
+        setErrorMsg("Something bad happened: " + error);
+        console.log("Something bad happened: " + error);
       }
     }
   };
