9b374b7b1852df935d806a0e1974fe0d3c15bbcd
Kevin Johnson <kevin@kj800x.com>
2024-10-01 23:35:00 +0100

    Don't change this

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ pushd frontend/brainstorm-shuffle-app
 npm install
 npm run build
 popd
-docker build -f Dockerfile.backend -t local/brainstorm-shuffle-backend
-docker build -f Dockerfile.nginx -t local/brainstorm-shuffle-nginx
-docker run -d -p 8000:8000 -v ./backend:/app --name brainstorm-shuffle-backend local/brainstorm-shuffle-backend
-docker run -d -p 8080:80 -v ./frontend/brainstorm-shuffle-app/dist:/config/www -v ./nginx.conf:/config/nginx/site-confs/default.conf --name brainstorm-shuffle-nginx local/brainstorm-shuffle-nginx
+podman build -f Dockerfile.backend -t local/brainstorm-shuffle-backend
+podman build -f Dockerfile.nginx -t local/brainstorm-shuffle-nginx
+podman run -d -p 8000:8000 -v ./backend:/app --name brainstorm-shuffle-backend local/brainstorm-shuffle-backend
+podman run -d -p 8080:80 -v ./frontend/brainstorm-shuffle-app/dist:/config/www -v ./nginx.conf:/config/nginx/site-confs/default.conf --name brainstorm-shuffle-nginx local/brainstorm-shuffle-nginx
 
